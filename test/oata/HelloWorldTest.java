05819d1b5173b6aad24903f563c678f06734addc
Jianwei Cui <jianwcui@amazon.com>
2018-10-23 01:35:24 +0000

    Bulk meat

---
+++ test/oata/HelloWorldTest.java
@@ -0,0 +1,21 @@
+package oata;
+
+import org.junit.Test;
+
+//import static org.junit.Assert.fail;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testNothing() {
+        System.out.println("Ha");
+        return;
+    }
+
+    @Test
+    public void testWillAlwaysFail() {
+        //fail("An error message");
+        return;
+    }
+
+}
